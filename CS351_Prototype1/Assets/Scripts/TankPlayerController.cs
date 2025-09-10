367f18b6e0009941e8fce2191413f315a4cfc632
BenjaminLockard <BenjaminLockardBG@gmail.com>
2025-09-10 15:31:42 -0500

    9/10

---
+++ CS351_Prototype1/Assets/Scripts/TankPlayerController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankPlayerController : MonoBehaviour
+{
+    // try setting to 8 in inspect - holds move speed
+    public float mSpeed;
+    // try setting to 100 in inspect - holds turn speed
+    public float tSpeed;
+
+    public float verticalInput;
+    public float horizontalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector2.right * Time.deltaTime * mSpeed * verticalInput);
+
+        transform.Rotate(Vector3.back, Time.deltaTime * tSpeed * horizontalInput);
+
+
+
+    }
+}
