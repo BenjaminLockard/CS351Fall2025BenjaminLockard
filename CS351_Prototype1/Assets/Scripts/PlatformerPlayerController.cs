d727c723bdeda5c486600dc64cc92e4d565733bd
BenjaminLockard <BenjaminLockardBG@gmail.com>
2025-09-22 15:27:06 -0500

    9/22 - Error

---
+++ CS351_Prototype1/Assets/Scripts/PlatformerPlayerController.cs
@@ -0,0 +1,71 @@
+/* Author: Benjamin Lockard
+ * Date: 9/22/2025
+ * Assignment: Platformer Follow Along
+ * Description: Controls platformer player unit
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformerPlayerController : MonoBehaviour
+{
+    //Player movement speed
+    public float moveSpeed = 5f;
+
+    // Jump potency
+    public float jumpForce = 10f;
+
+    // detect ground
+    public LayerMask groundLayer;
+
+    //transform for ground-detecting position
+    public Transform groundCheck;
+    public float groundCheckRadius;
+
+    //References rigid body
+    private Rigidbody2D rb;
+    // horisontal var
+    private float horizontalInput;
+    //vertical var
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        if(groundCheck = null)
+        {
+            Debug.LogError("groundCheck unassigned to player controller.");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.velocity = newVector2(rb.velocity.x, jumpForce);
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+        if (horizontalInput > 0)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f); //right
+        }
+        else if (horizontalInput < 0)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f); //left
+        }
+    }
+
+}
