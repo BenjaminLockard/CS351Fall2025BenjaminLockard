8f3c17ae646579628936818d8ea031b403f0acc3
BenjaminLockard <BenjaminLockardBG@gmail.com>
2025-09-26 15:26:02 -0500

    9/26 - TopDown

---
+++ CS351_Prototype1/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DialogueManager : MonoBehaviour
+{
+    public TMP_Text textbox;
+
+    public string[] sentences;
+    private int index;
+    public float typingSpeed;
+
+    void OnEnable()
+    {
+        StartCoroutine(Type());
+    }
+
+    IEnumerator Type()
+    {
+        textbox.text = "";
+        foreach(char letter in sentences[index])
+        {
+            textbox.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+
+    public void NextSentence()
+    {
+        if (index < sentences.Length - 1)
+        {
+            index++;
+            textbox.text = "";
+            StartCoroutine(Type());
+        } else
+        {
+            textbox.text = "";
+        }
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
